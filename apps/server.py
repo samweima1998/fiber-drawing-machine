aec3824572ce6b52ba14449d325a1d1e2dc92997
samweima1998 <samweima1998@gmail.com>
2025-07-30 13:00:29 +0200

    Filter out /status calls from log

--- apps/server.py
+++ apps/server.py
@@ -396,4 +396,4 @@ async def serve_svelte():
 app.mount("/", StaticFiles(directory=svelte_frontend, html=True), name="build")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7070, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=7070, log_level="info", access_log=False)
